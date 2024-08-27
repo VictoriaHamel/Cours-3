b6aac890a74985f25f1983b4fd6431c67fc73e0f
6243484 <6243484@cstjean.qc.ca>
2024-08-27 14:07:06 -0400

    2

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ class Node:
         self.value = data
         self.left = None
         self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+
+tree = BinaryTree()
+
+noeud1 = Node(1)
+noeud2 = Node(2)
+noeudA = Node('a')
+noeudB = Node('b')
+noeud2.left = noeudA
+noeud2.right = noeudB
+noeud3 = Node(3)
+
+tree.root = noeud1
+noeud1.left = noeud2
+noeud1.right = noeud3
